c4cf2d0e6d4c1fe7eb6a506f6fa927b1b916dee9
Zeru Hu <green2onion@gmail.com>
2020-10-13 23:10:09 +0800

    Grid generation

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    GameManager gameManager;
+    GameObject[,] grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        grid = gameManager.grid;
+        transform.position = new Vector3(grid[grid.GetLength(0) / 2, grid.GetLength(0) / 2].transform.position.x, grid[grid.GetLength(0) / 2, grid.GetLength(0) / 2].transform.position.y, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[,] grid;
+    public GameObject tile;
+    public int gridRow;
+    public int gridCol;
+    public GameObject player;
+    void SpawnGrid()
+    {
+        grid = new GameObject[gridRow, gridCol];
+        for (int x = 0; x < gridRow; x++)
+        {
+            for (int y = 0; y < gridCol; y++)
+            {
+                grid[x, y] = Instantiate(tile, new Vector3((x - y) * 1, (x + y) * 0.7f), new Quaternion());
+                grid[x, y].GetComponentInChildren<TextMesh>().text = x.ToString() + "," + y.ToString();
+                /* screen.x = (map.x - map.y) * TILE_WIDTH_HALF;
+                   screen.y = (map.x + map.y) * TILE_HEIGHT_HALF;
+                */
+            }
+        }
+    }
+    void SpawnPlayer()
+    {
+        player = Instantiate(player, grid[1, 1].transform);
+    }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SpawnGrid();
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
